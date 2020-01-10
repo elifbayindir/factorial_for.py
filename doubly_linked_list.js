8683ef84f7cc7ac3176cdaa9845d47831cc57354
elifbayindir <47335074+elifbayindir@users.noreply.github.com>
2020-01-10 11:29:37 +0300

    Create doubly_linked_list.js

---
+++ doubly_linked_list.js
@@ -0,0 +1,107 @@
+// Implementation of Doubly Linked List 
+// I used pop and shift methods in remove method
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    pop(){ // removes a node from the end 
+        if(!this.head) return undefined;
+        var poppedNode = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
+    }
+
+    shift(){ // removes a node from the beginning
+        if(this.length === 0) return undefined;
+        var oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
+    insert(val){ // adds a new node to the begnning
+        var newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    traverse(){ // prints all elements
+        var current = this.head;  
+        while(current){ 
+            console.log(current.val); 
+	    current = current.next; 
+        }        
+    }
+
+    find(index){ // retrieves a node by it's position
+        if(index < 0 || index >= this.length) return null;
+        var count, current;
+        if(index <= this.length/2){
+            count = 0;
+            current = this.head;
+            while(count !== index){
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while(count !== index){
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
+
+    remove(index){ // removes a node from the linked list at a specific position
+        if(index < 0 || index >= this.length) return undefined;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        var removedNode = this.find(index);
+        removedNode.prev.next = removedNode.next;
+        removedNode.next = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;        
+        this.length--;
+        return removedNode;
+    }
+}
+
+var list = new DoublyLinkedList()
+
